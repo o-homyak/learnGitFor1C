09f8d349b591b42de82f42b718382fe021200db2
o-homyak <oleksandrhomyak@gmail.com>
2016-10-04 19:45:30 +0300

    Настройка каталогов

---
+++ doc/pickles/pickledFeatures.js
@@ -0,0 +1,86 @@
+jsonPWrapper ({
+  "Features": [
+    {
+      "RelativeFolder": "РасчетВозрастаСотрудникаВМесяцах.feature",
+      "Feature": {
+        "Name": "Расчет возраста сотрудника в месяцах",
+        "Description": "\tКак Кадровик\r\n\tХочу Получать возраст человека в месяцах\r\n\tЧтобы экономить время",
+        "FeatureElements": [
+          {
+            "Name": "Возраст по дате рождения '20150428'",
+            "Slug": "20150428",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Допустим ",
+                "Name": "у меня есть человек с датой рождения '20150428'",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Тогда ",
+                "Name": "результат должен составить 17 месяцев",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": false,
+          "WasSuccessful": false
+        },
+        "Tags": []
+      },
+      "Result": {
+        "WasExecuted": false,
+        "WasSuccessful": false
+      }
+    }
+  ],
+  "Summary": {
+    "Tags": [],
+    "Folders": [
+      {
+        "Folder": "РасчетВозрастаСотрудникаВМесяцах.feature",
+        "Total": 1,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 1
+      }
+    ],
+    "NotTestedFolders": [
+      {
+        "Folder": "РасчетВозрастаСотрудникаВМесяцах.feature",
+        "Total": 0,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "Scenarios": {
+      "Total": 1,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 1
+    },
+    "Features": {
+      "Total": 1,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 1
+    }
+  },
+  "Configuration": {
+    "SutName": "learn",
+    "SutVersion": "0.1",
+    "GeneratedOn": "3 октября 2016 11:53:23"
+  }
+});
